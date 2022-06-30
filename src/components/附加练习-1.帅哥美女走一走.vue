cab2e51335719748c55628415ce7ab892f6f6c86
Yi3512 <2399283079@qq.com>
2022-06-30 19:50:51 +0800

    附加练习-1.帅哥美女走一走

---
+++ src/components/附加练习-1.帅哥美女走一走.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in navs" :key="item">{{ item }}</li>
+    </ul>
+    <button @click="btn">走一走</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navs: ['帅哥', '程序员', '美女'],
+    };
+  },
+  methods: {
+    btn() {
+      this.navs.push(this.navs[0]);
+      this.navs.shift();
+    },
+  },
+};
+</script>
+
+<style></style>
